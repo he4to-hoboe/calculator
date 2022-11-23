b9c7da387f6fe5aea8939eb1b813b1c7299d6979
Vladislav Klevtsov <klevtsov1204@mail.ru>
2022-11-23 18:19:28 +0300

    first version v0.1

---
+++ calculator.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL,"Rus");
+    double a,b,c;
+    int z;
+    cout << "Первое число: ";
+    cin >> a;
+    cout <<"Второе число: ";
+    cin >> b;
+    cout <<"Выберите операцию 1(+) 2(-) 3(*) 4(/)";
+    cin >> z;
+    switch (z)
+    {
+        case '1':   c=a+b;
+                    break;
+ 
+        case '2':   c=a-b;
+                    break;
+ 
+        case '3':   c=a*b;
+                    break;
+ 
+        case '4':   c=a/b;
+                    break;
+ 
+        default:    cout <<"Не верное значение."; 
+    }
+    cout << "результат: " << c << endl;
+system("PAUSE");
+}
