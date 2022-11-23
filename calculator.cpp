57cb5639015551dd61ee9ae560c28ad13ad8d38d
Vladislav Klevtsov <klevtsov1204@mail.ru>
2022-11-23 21:16:30 +0300

    added do-while

--- calculator.cpp
+++ calculator.cpp
@@ -4,13 +4,17 @@ using namespace std;
 
 int main()
 {
+    char choise;
     double a, b;
-
     setlocale(LC_ALL, "Rus");
-    cout << "Первое число: ";
-    cin >> a;
-    cout << "Второе число: ";
-    cin >> b;
-    fswitch(a, b);
+    do {
+         cout << "Первое число: ";
+        cin >> a;
+        cout << "Второе число: ";
+        cin >> b;
+        fswitch(a, b);
+        cout << "Продоллжаем? Y-да, N-нет";
+            cin >> choise;
+    } while (choise == 'Y' || choise == 'y');
     system("PAUSE");
 }
